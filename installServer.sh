8307be78d7b961db5dcd8bab5c1dd6b2d7c29d9a
sunliang <sunliang711@163.com>
2018-11-18 16:58:52 +0800

    sed substitute cmd use | as separator ,because path has '/' char

--- installServer.sh
+++ installServer.sh
@@ -10,7 +10,7 @@ fi
 sed "s|ROOT|$root|g" ssrserver >/usr/local/bin/ssrserver
 chmod +x /usr/local/bin/ssrserver
 
-sed "s/ROOT/$root/" ssrserver.service > /etc/systemd/system/ssrserver.service
+sed "s|ROOT|$root|g" ssrserver.service > /etc/systemd/system/ssrserver.service
 systemctl enable ssrserver
 echo "use ssrserver config to config ssrserver,then issue systemctl start ssrserver"
 #TODO install libsodium if needed
